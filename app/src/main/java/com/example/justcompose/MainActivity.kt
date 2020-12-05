bf5452f235cf62927f910b11e0a561c69ef042b5
dragon <742420210@qq.com>
2020-12-05 11:26:09 +0800

    尝试OutlinedTextField

--- app/src/main/java/com/example/justcompose/MainActivity.kt
+++ app/src/main/java/com/example/justcompose/MainActivity.kt
@@ -31,6 +31,8 @@ fun mainView() {
 
         TextFieldDemo()
 
+        OutlinedTextFieldDemo()
+
         ButtonDemo()
 
         IconDemo()
--- app/src/main/java/com/example/justcompose/components/TextField.kt
+++ app/src/main/java/com/example/justcompose/components/TextField.kt
@@ -2,7 +2,9 @@ package com.example.justcompose.components
 
 import android.util.Log
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Icon
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.material.icons.Icons
@@ -16,6 +18,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
 
 
@@ -25,32 +28,57 @@ fun TextFieldDemo() {
     var text by savedInstanceState { "" }
 
     TextField(
-            value = text,
-            label = {
-                Text("标签")
-            },
-            placeholder = {
-                Text("请输入内容")
-            },
-            leadingIcon = {
-                Icon(Icons.Filled.Favorite)
-            },
-            trailingIcon = {
-                Icon(
-                        asset = Icons.Filled.Delete,
-                        modifier = Modifier.clickable(
-                                onClick = {
-                                    Log.e("TextField", "点击了尾部标签")
-                                    text = ""
-                                }))
-            },
-            onValueChange = {
-                text = it
-            })
+        value = text,
+//        label = {
+//            Text("标签")
+//        },
+        placeholder = {
+            Text("请输入内容")
+        },
+//            leadingIcon = {
+//                Icon(Icons.Filled.Favorite)
+//            },
+//            trailingIcon = {
+//                Icon(
+//                        asset = Icons.Filled.Delete,
+//                        modifier = Modifier.clickable(
+//                                onClick = {
+//                                    Log.e("TextField", "点击了尾部标签")
+//                                    text = ""
+//                                }))
+//            },
+        onValueChange = {
+            text = it
+        },
+        modifier = Modifier.padding(8.dp)
+    )
 }
 
 @Preview
 @Composable
 fun previewTextFieldDemo() {
     TextFieldDemo()
+}
+
+@Composable
+fun OutlinedTextFieldDemo() {
+    var text by savedInstanceState { "" }
+
+    OutlinedTextField(
+        value = text,
+        placeholder = {
+            Text("请输入内容")
+        },
+        onValueChange = { text = it },
+//        label = {
+//            Text("Label")
+//        },
+        modifier = Modifier.padding(8.dp)
+    )
+}
+
+@Preview
+@Composable
+fun previewOutlinedTextFieldDemo() {
+    OutlinedTextFieldDemo()
 }
